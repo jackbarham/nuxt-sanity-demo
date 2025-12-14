ac05b49ad522ef0650c6efe0753ff815c6522aba
Jack Barham <>
2025-12-14 11:29:48 +0000

    Install Tailwind

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,13 +1,20 @@
 import { fileURLToPath } from 'url'
+import tailwindcss from "@tailwindcss/vite"
 
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
+  compatibilityDate: '2025-05-15',
   devtools: { enabled: true },
+  css: ['./app/assets/css/main.css'],
   modules: ['@nuxtjs/sanity'],
   sanity: {
     projectId: process.env.NUXT_SANITY_PROJECT_ID,
     dataset: process.env.NUXT_SANITY_DATASET,
     apiVersion: process.env.NUXT_SANITY_API_VERSION,
+  },
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ],
   },
   alias: {
     '@sanity/visual-editing': fileURLToPath(new URL('./mocks/visual-editing.js', import.meta.url)),
